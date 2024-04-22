066cedbd8fd954192c26a33711843b31a268c4ee
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2024-04-22 21:10:51 +0200

    replace light and rm some obj

--- src/experience/World/Attic.js
+++ src/experience/World/Attic.js
@@ -66,7 +66,7 @@ export default class Attic {
                     child.material = this.materialLibrary.getCarpetMaterial()
                 } else if (name.includes("walkman") || name.includes("tv") || name.includes("cassette") || name.includes("cartes_postales") || name.includes("projecteur")) {
                     child.material = this.materialLibrary.getTmpInteractionMaterial()
-                }  else if (name.includes("disque")) {
+                }  else if (name.includes("disque") || name.includes("drap")) {
                     // this.godRayMeshs.push(child)
                     child.visible = false
                 }
