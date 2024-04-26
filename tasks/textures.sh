dd41ba912e28cfa768771d8f104095f4cd0002b2
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2024-04-27 00:38:36 +0200

    added textures script

--- tasks/textures.sh
+++ tasks/textures.sh
@@ -3,51 +3,45 @@
 dir="public/assets/textures/attic/ground"
 size=1024
 
-# Assurez-vous que le chemin spécifié existe
 if [ ! -d "$dir" ]; then
   echo "Le répertoire spécifié n'existe pas."
   exit 1
 fi
 
-# Boucle sur les fichiers avec des extensions spécifiques
 find "$dir" -type f \( -iname "*.jpg" -o -iname "*.png" \) | while IFS= read -r file; do
     echo "Traitement de $file"
     directory=$(dirname "$file")
 
-    # Définir le nouveau nom en fonction du type de texture
     case "$file" in
-            *"_color"* | *"_basecolor"*)
-                new_filename="diffuse.webp"
-                ;;
-            *"_height"*)
-                new_filename="displacement.webp"
-                ;;
-            *"_normal"*)
-                new_filename="normal.webp"
-                ;;
-            *"_roughness"*)
-                new_filename="roughness.webp"
-                ;;
-            *)
-                # Utiliser un nom générique si aucun motif n'est trouvé
-                base=$(basename "${file%.*}")
-                new_filename="${base}.webp"
-                ;;
-        esac
+        *"_color"* | *"_basecolor"*)
+            new_filename="diffuse.webp"
+            ;;
+        *"_height"*)
+            new_filename="displacement.webp"
+            ;;
+        *"_normal"*)
+            new_filename="normal.webp"
+            ;;
+        *"_roughness"*)
+            new_filename="roughness.webp"
+            ;;
+        *"_ao"*)
+                    new_filename="ao.webp"
+                    ;;
+        *)
+            base=$(basename "${file%.*}")
+            new_filename="${base}.webp"
+            ;;
+    esac
 
-    # Utiliser magick pour obtenir les dimensions de l'image
-    read width height <<< $(magick identify -format "%wx%h" "$file")
+    read width height <<< $(magick identify -format "%w %h" "$file")
 
-    # Vérifier si l'image doit être redimensionnée
     if [ "$width" -gt $size ] || [ "$height" -gt $size ]; then
-        # Redimensionner et convertir l'image
         magick convert "$file" -resize "${size}x${size}" "${directory}/${new_filename}"
     else
-        # Convertir l'image sans redimensionnement
         magick convert "$file" "${directory}/${new_filename}"
     fi
 
-    # Supprimer le fichier original après conversion
     rm "$file"
 done
 
