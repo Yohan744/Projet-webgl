efa3be2f9188fe1565969aa34e08dbaec2fc99f3
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2024-05-11 21:42:13 +0200

    test new animation drag

--- src/experience/World/Props/Essential/Projector.js
+++ src/experience/World/Props/Essential/Projector.js
@@ -1,20 +1,35 @@
 import * as THREE from "three";
 import Experience from "../../../Experience";
+import gsap from "gsap";
 
 export default class Projector {
     constructor() {
         this.experience = new Experience();
         this.scene = this.experience.scene;
         this.camera = this.experience.camera.instance;
-        this.renderer = this.experience.renderer.instance;
-        this.clock = new THREE.Clock();
+        this.pointer = this.experience.pointer;
+        this.appStore = this.experience.appStore;
+
         this.animations = [];
+
         this.textures = [
             this.experience.resources.items.monasurf,
             this.experience.resources.items.monabouquet,
             this.experience.resources.items.backgroundTreeTexture
         ];
-        this.textureIndex = 0;
+
+        this.isDragging = false;
+        this.isAnimating = false;
+        this.isFirstAnimationIsDone = false;
+
+        this.mouseStartClickPosition = {
+            x: 0,
+            y: 0,
+        };
+
+        this.dragDistance = 0.3
+
+        this.setEvents();
         this.init();
     }
 
@@ -22,51 +37,68 @@ export default class Projector {
         this.projectorModel = this.experience.resources.items.projectorModel.scene;
         this.projectorModel.position.set(-3.8, 1.15, 4.5);
         this.scene.add(this.projectorModel);
+    }
 
-        this.experience.pointer.on('click', this.onPointerDown.bind(this), false);
-        this.experience.pointer.on('movement', this.onPointerMove.bind(this), false);
-        this.experience.pointer.on('click-release', this.onPointerUp.bind(this), false);
-
-        this.dragging = false;
-        this.animate();
+    setEvents() {
+        this.pointer.on('click', this.onPointerDown.bind(this));
+        this.pointer.on('movement-orbit', this.onPointerMove.bind(this));
+        this.pointer.on('click-release', this.onPointerUp.bind(this));
     }
 
     onPointerDown() {
-        const mousePosition = this.experience.pointer.getMousePosition();
-        this.experience.pointer.raycaster.setFromCamera(mousePosition, this.camera);
-        const intersects = this.experience.pointer.raycaster.intersectObjects([this.projectorModel], true);
+        const mousePosition = this.pointer.getMousePosition();
+        this.pointer.raycaster.setFromCamera(mousePosition, this.camera);
+        const intersects = this.pointer.raycaster.intersectObjects([this.projectorModel], true);
 
         if (intersects.length > 0 && intersects[0].object.name === 'tireuse') {
-            this.dragging = true;
+            this.isDragging = true;
             this.draggableModel = intersects[0].object;
-            this.startY = mousePosition.y;
-            this.startZ = this.draggableModel.position.z;
-            console.log(`dragging a commencé  Z: ${this.startZ}`);
+            this.mouseStartClickPosition = {
+                x: mousePosition.x,
+                y: mousePosition.y,
+            };
+            console.log("ready")
+            this.appStore.updateInteractingState(true)
         }
     }
 
-    onPointerMove() {
-        if (!this.dragging || !this.draggableModel) return;
+    onPointerMove(mouse) {
+        if (!this.isDragging || !this.draggableModel || this.isAnimating) return;
 
-        const currentMouseY = this.experience.pointer.getMousePosition().y;
-        const deltaY = currentMouseY - this.startY;
-        const newZ = this.startZ + deltaY * 0.01;
-        this.draggableModel.position.z = THREE.MathUtils.clamp(newZ, 0, 0.5);
-        console.log(`deltaY: ${deltaY}, newZ: ${newZ}, clampedZ: ${this.draggableModel.position.z}`);
-        this.startY = currentMouseY;
+        if (!this.isFirstAnimationIsDone && (mouse.x + 1) - (this.mouseStartClickPosition.x + 1) > this.dragDistance) {
+            console.log("Parfait")
+            this.isAnimating = true
+            gsap.to(this.draggableModel.position, {
+                z: "+=0.05",
+                duration: 1,
+                onComplete: () => {
+                    this.isAnimating = false
+                    this.isFirstAnimationIsDone = true
+                }
+            })
+        } else if (this.isFirstAnimationIsDone && (this.mouseStartClickPosition.x + 1) - (mouse.x + 1) > this.dragDistance) {
+            console.log("Parfait")
+            this.isAnimating = true
+            gsap.to(this.draggableModel.position, {
+                z: "-=0.05",
+                duration: 1,
+                onComplete: () => {
+                    this.isAnimating = false
+                    this.isFirstAnimationIsDone = false
+                }
+            })
+
+        } else {
+            console.log("pas assez loin")
+        }
     }
 
     onPointerUp() {
-        if (this.dragging) {
-            console.log(`relaché Z: ${this.draggableModel.position.z}`);
-            this.dragging = false;
+        if (this.isDragging) {
+            this.isDragging = false;
             this.draggableModel = null;
+            this.appStore.updateInteractingState(false)
         }
     }
 
-
-    animate() {
-        requestAnimationFrame(this.animate.bind(this));
-        this.renderer.render(this.scene, this.camera);
-    }
 }
