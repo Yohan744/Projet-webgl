accd732d311d4228b17024d5792e2f3e1b414aee
LucieEditionsTissot <llesnier@editions-tissot.fr>
2024-05-12 15:20:28 +0200

    add projector v2 complete interactions

--- src/experience/World/Props/Essential/Projector.js
+++ src/experience/World/Props/Essential/Projector.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import {DoubleSide} from "three";
 import Experience from "../../../Experience";
 import gsap from "gsap";
 
@@ -21,6 +22,7 @@ export default class Projector {
         this.isDragging = false;
         this.isAnimating = false;
         this.isFirstAnimationIsDone = false;
+        this.textureIndex = 0;
 
         this.mouseStartClickPosition = {
             x: 0,
@@ -28,6 +30,10 @@ export default class Projector {
         };
 
         this.dragDistance = 0.3
+        this.isSpotlight = false;
+        this.pointerDown = this.onPointerDown.bind(this);
+        this.pointerMove = this.onPointerMove.bind(this);
+        this.pointerUp = this.onPointerUp.bind(this);
 
         this.setEvents();
         this.init();
@@ -40,25 +46,116 @@ export default class Projector {
     }
 
     setEvents() {
-        this.pointer.on('click', this.onPointerDown.bind(this));
-        this.pointer.on('movement-orbit', this.onPointerMove.bind(this));
-        this.pointer.on('click-release', this.onPointerUp.bind(this));
+        this.pointer.on('click', this.pointerDown );
+        this.pointer.on('movement-orbit', this.pointerMove);
+        this.pointer.on('click-release', this.pointerUp);
     }
 
     onPointerDown() {
         const mousePosition = this.pointer.getMousePosition();
         this.pointer.raycaster.setFromCamera(mousePosition, this.camera);
         const intersects = this.pointer.raycaster.intersectObjects([this.projectorModel], true);
+        if (intersects.length > 0) {
+            console.log(intersects[0].object.name)
+            if (!this.isCameraMoved) {
+                this.onModelClicked(intersects[0]);
+                this.isCameraMoved = true;
+            } else {
+                if (intersects[0].object.name === 'tireuse') {
+                    this.isDragging = true;
+                    this.draggableModel = intersects[0].object;
+                    this.mouseStartClickPosition = {
+                        x: mousePosition.x,
+                        y: mousePosition.y,
+                    };
+                    console.log("ready")
+                    this.appStore.updateInteractingState(true)
+                } else if (intersects[0].object.name === 'EnsembleRail' && this.isFirstAnimationIsDone) {
+                    this.moveRail();
+                } else if(intersects[0].object.name === 'BoutonOn') {
+                    if (!this.isSpotlight) {
+                        const button = intersects[0].object
+                        gsap.to(button.position, {
+                            y: "-=0.003",
+                            duration: 0.5,
+                            onComplete: () => {
+                                this.setupSpotlight(true);
+                                gsap.to(button.position, {
+                                    y: "+=0.003",
+                                    duration: 0.5,
+                                });
+                            }
+                        });
+                    } else {
+                            const button = intersects[0].object
+                            gsap.to(button.position, {
+                                y: "-=0.003",
+                                duration: 0.5,
+                                onComplete: () => {
+                                   this.removeSpotlight()
+                                    gsap.to(button.position, {
+                                        y: "+=0.003",
+                                        duration: 0.5,
+                                    });
+                                }
+                            });
+                    }
+                }
+            }
+        }
+    }
+    onModelClicked(intersect) {
+        const offset = new THREE.Vector3(-1.3, 0.28, 0.5);
+        const modelPosition = intersect.object.getWorldPosition(new THREE.Vector3());
+        const targetPosition = modelPosition.clone().add(offset);
+        this.experience.camera.lookAtSheet(targetPosition);
+        this.isCameraMoved = true;
+    }
 
-        if (intersects.length > 0 && intersects[0].object.name === 'tireuse') {
-            this.isDragging = true;
-            this.draggableModel = intersects[0].object;
-            this.mouseStartClickPosition = {
-                x: mousePosition.x,
-                y: mousePosition.y,
-            };
-            console.log("ready")
-            this.appStore.updateInteractingState(true)
+    setupSpotlight(withTexture) {
+        this.removeSpotlight();
+
+        let spotlight = new THREE.SpotLight(0xffffff, 60, 0, Math.PI * 0.05);
+        spotlight.position.copy(this.projectorModel.position);
+        spotlight.target.position.set(this.projectorModel.position.x + 0.7, this.projectorModel.position.y + 0.02, this.projectorModel.position.z);
+
+        spotlight.angle = 0.1;
+        spotlight.penumbra = 0.7;
+        spotlight.decay = 1;
+        spotlight.distance = 0;
+
+        spotlight.castShadow = true;
+        spotlight.shadow.mapSize.width = 1024;
+        spotlight.shadow.mapSize.height = 1024;
+        spotlight.shadow.camera.near = 1;
+        spotlight.shadow.camera.far = 100;
+        spotlight.shadow.camera.fov = 30;
+        spotlight.shadow.camera.aspect = 6;
+        spotlight.shadow.camera.focus = 0.6;
+
+        if (withTexture) {
+            const texture = this.textures[this.textureIndex];
+            texture.flipY = true;
+            spotlight.map = texture;
+            spotlight.map.side = DoubleSide;
+        }
+
+        this.scene.add(spotlight.target);
+        this.scene.add(spotlight);
+        this.isSpotlight = true;
+
+        if (withTexture) {
+            this.spotlightWithTexture = spotlight;
+        } else {
+            this.spotlight = spotlight;
+        }
+    }
+
+    removeSpotlight() {
+        if (this.spotlightWithTexture) {
+            this.scene.remove(this.spotlightWithTexture);
+            this.spotlightWithTexture = null;
+            this.isSpotlight = false;
         }
     }
 
@@ -66,33 +163,85 @@ export default class Projector {
         if (!this.isDragging || !this.draggableModel || this.isAnimating) return;
 
         if (!this.isFirstAnimationIsDone && (mouse.x + 1) - (this.mouseStartClickPosition.x + 1) > this.dragDistance) {
-            console.log("Parfait")
-            this.isAnimating = true
+            console.log("Parfait");
+            this.isAnimating = true;
             gsap.to(this.draggableModel.position, {
                 z: "+=0.05",
                 duration: 1,
                 onComplete: () => {
-                    this.isAnimating = false
-                    this.isFirstAnimationIsDone = true
+                    this.isAnimating = false;
+                    this.isFirstAnimationIsDone = true;
                 }
-            })
+            });
+            this.animateTextureDisappearance();
         } else if (this.isFirstAnimationIsDone && (this.mouseStartClickPosition.x + 1) - (mouse.x + 1) > this.dragDistance) {
-            console.log("Parfait")
-            this.isAnimating = true
+            console.log("Parfait");
+            this.isAnimating = true;
+                this.changeTextureAnimation();
             gsap.to(this.draggableModel.position, {
                 z: "-=0.05",
                 duration: 1,
                 onComplete: () => {
-                    this.isAnimating = false
-                    this.isFirstAnimationIsDone = false
+                    this.isAnimating = false;
+                    this.isFirstAnimationIsDone = false;
                 }
-            })
-
+            });
         } else {
-            console.log("pas assez loin")
+            console.log("pas assez loin");
         }
     }
 
+    moveRail() {
+        const rail = this.projectorModel.getObjectByName('rail_Diapo');
+        if (rail) {
+            gsap.to(rail.position, {
+                x: "+=0.005",
+                duration: 1,
+                onComplete: () => {
+                    console.log("Rail animation complete");
+                }
+            });
+        }
+    }
+
+    animateTextureDisappearance() {
+        if(this.isSpotlight) {
+            gsap.to(this.spotlightWithTexture.position, {
+                z: "+=0.05",
+                duration: 0.3,
+                ease: "power2.in",
+                onComplete: () => {
+                    this.spotlightWithTexture.position.z -= 0.05;
+                    this.spotlightWithTexture.visible = false;
+                    this.setupSpotlight(false);
+                }
+            });
+        }
+    }
+
+    changeTextureAnimation() {
+        this.textureIndex = (this.textureIndex + 1) % this.textures.length;
+        const newTexture = this.textures[this.textureIndex];
+        newTexture.flipY = true;
+        if(this.isSpotlight) {
+            this.spotlight.visible = false;
+            if (!this.spotlightWithTexture) {
+                this.setupSpotlight(true);
+                this.spotlightWithTexture.position.z += 0.05;
+                gsap.to(this.spotlightWithTexture.map, {
+                    onComplete: () => {
+                        this.textureIndex = newTexture;
+                        this.isAnimating = false;
+                        gsap.to(this.spotlightWithTexture.position, {
+                            z: "-=0.05",
+                            duration: 0.5,
+                        });
+                    }
+                });
+                this.spotlightWithTexture.map = newTexture;
+            }
+        }
+    }
     onPointerUp() {
         if (this.isDragging) {
             this.isDragging = false;
@@ -101,4 +250,26 @@ export default class Projector {
         }
     }
 
+    destroy() {
+        if (this.spotlightWithTexture) {
+            this.scene.remove(this.spotlightWithTexture);
+            if (this.spotlightWithTexture.map) {
+                this.spotlightWithTexture.map.dispose();
+            }
+            this.spotlightWithTexture.material.dispose();
+            this.spotlightWithTexture = null;
+        }
+
+        if (this.spotlight) {
+            this.scene.remove(this.spotlight);
+            this.spotlight.material.dispose();
+            this.spotlight = null;
+        }
+
+        this.pointer.off('click', this.onPointerDown.bind(this));
+        this.pointer.off('movement-orbit', this.onPointerMove.bind(this));
+        this.pointer.off('click-release', this.onPointerUp.bind(this));
+    }
+
 }
+
