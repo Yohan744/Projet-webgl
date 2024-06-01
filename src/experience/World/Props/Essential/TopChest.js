245f9f0453fb1bfcfd0239b1d4cc20955faaf116
LucieEditionsTissot <llesnier@editions-tissot.fr>
2024-06-01 13:39:18 +0200

    add cassette in walkman

--- src/experience/World/Props/Essential/TopChest.js
+++ src/experience/World/Props/Essential/TopChest.js
@@ -1,7 +1,6 @@
 import * as THREE from "three";
 import Experience from "../../../Experience";
 import Prop from "../Prop";
-import {useInteractableObjects} from "../../ObjectsInteractable";
 
 export default class TopChest extends Prop {
     constructor(mesh, desiredRotationOnClick = new THREE.Vector3(0, 0, 0), animatePropsToCameraOnClick = true, distanceToCamera = 0.6, isOutlined = 1.05, propSound) {
@@ -11,22 +10,52 @@ export default class TopChest extends Prop {
         this.resources = this.experience.resources;
         this.scene = this.experience.scene;
 
+        this.mixer = null;
+        this.actions = {};
+
+        this.init();
     }
 
     init() {
-
+        this.setupAnimations();
     }
 
-    onClickGeneral() {
+    setupAnimations() {
+        const animations = this.mesh.animations;
 
+        if (animations && animations.length > 0) {
+            this.mixer = new THREE.AnimationMixer(this.mesh);
+            animations.forEach(animation => {
+                this.actions[animation.name] = this.mixer.clipAction(animation);
+            });
+        } else {
+            console.warn("No animations found on the mesh.");
+        }
     }
 
     onClick() {
-        console.log("click top chest")
+        console.log("click top chest");
+        this.playAnimation("AnimationName");
+    }
+
+    playAnimation(animationName) {
+        if (this.actions[animationName]) {
+            this.actions[animationName].reset().play();
+        } else {
+            console.warn(`Animation ${animationName} not found.`);
+        }
+    }
+
+    update(delta) {
+        if (this.mixer) {
+            this.mixer.update(delta);
+        }
     }
 
     destroy() {
-        super.destroy()
+        super.destroy();
+        if (this.mixer) {
+            this.mixer.stopAllAction();
+        }
     }
-
 }
